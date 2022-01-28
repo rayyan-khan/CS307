b537ced921fe7110daddfa08ae4a4c161feab187
junaidjaved <JunaidJ.2002@gmail.com>
2022-01-28 11:03:41 -0500

    route-setup

---
+++ front-end/src/react-redux/usernameSlice.js
@@ -0,0 +1,18 @@
+// import { createSlice } from '@reduxjs/toolkit'
+
+// export const usernameSlice = createSlice({
+//   name: 'username',
+//   initialState: {
+//     value: "",
+//   },
+//   reducers: {
+//     assignUser: (state, action) => {
+//       state.value = action.payload
+//     },
+//   },
+// })
+
+// // Action creators are generated for each case reducer function
+// export const { assignUser } = usernameSlice.actions
+
+// export default usernameSlice.reducer
