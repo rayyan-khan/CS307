9a82631b567af7d55e3cdc71e55416917d43d8e6
junaidjaved <JunaidJ.2002@gmail.com>
2022-01-30 17:54:34 -0500

    removed redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { Provider } from 'react-redux'
-import {createStore} from 'redux'
-import reducer from './react-redux/reducers'
 import App from './App'
-
-let store = createStore(reducer)
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   rootElement
 );
 
