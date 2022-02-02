275da332c6fdf57035cb2a3555beee1475123dec
junaidjaved <JunaidJ.2002@gmail.com>
2022-02-01 22:39:55 -0500

    index.js update

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <App />,
-  rootElement
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+// 1. Create a client engine instance
+const engine = new Styletron();
+
+// 2. Provide the engine to the app
+// debug engine needs inlined source maps
+React.render(
+  <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+    <App />
+  </StyletronProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
