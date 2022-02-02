d7e617840cc83613b01417a40aef06151683d365
junaidjaved <JunaidJ.2002@gmail.com>
2022-02-02 16:57:28 -0500

    Added Posts and Homepage

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,16 +1,16 @@
-import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
-import { Client as Styletron } from "styletron-engine-atomic";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
 
-const debug =
-  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
-
-// 1. Create a client engine instance
-const engine = new Styletron();
-
-// 2. Provide the engine to the app
-// debug engine needs inlined source maps
-React.render(
-  <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <ChakraProvider>
     <App />
-  </StyletronProvider>
+  </ChakraProvider>,
+  rootElement
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals  
