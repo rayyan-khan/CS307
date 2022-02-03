4b5ed15fa07e79c285b64e880ed285e2394cd43c
Misha-Rahimi <mlrahimi@purdue.edu>
2022-02-02 20:12:10 -0500

    Added placeholder buttons

--- front-end/src/layouts/logged-in-user/profile.js
+++ front-end/src/layouts/logged-in-user/profile.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react'
 import logo from '../../logo.png';
+import '../../styles/profile.css'
 
 class Profile extends React.Component {
     constructor(props) {
@@ -17,18 +18,18 @@ class Profile extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('http://localhost:5000/api/dummy-get-profile/' + sessionStorage.getItem("username"))
-        .then(res => {
-            this.setState({
-                username: this.props.username,
-                bio: res.data.bio,
-                numTagsFollowing: this.formatNum(res.data.numTagsFollowing),
-                numFollowing: this.formatNum(res.data.numFollowing),
-                numFollowers: this.formatNum(res.data.numFollowers),
-                viewingSelf: sessionStorage.getItem('username') != null &&
-                    sessionStorage.getItem('username').localeCompare(this.props.username) === 0
+        var sessionUsername = sessionStorage.getItem("username")
+        axios.get('http://localhost:5000/api/dummy-get-profile/' + sessionUsername)
+            .then(res => {
+                this.setState({
+                    username: this.props.username,
+                    bio: res.data.bio,
+                    numTagsFollowing: this.formatNum(res.data.numTagsFollowing),
+                    numFollowing: this.formatNum(res.data.numFollowing),
+                    numFollowers: this.formatNum(res.data.numFollowers),
+                    viewingSelf: sessionUsername != null && sessionUsername.localeCompare(this.props.username) === 0
+                })
             })
-        })        
     }
 
     formatNum(num) {
@@ -38,18 +39,50 @@ class Profile extends React.Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header" style={{'justify-content': 'normal'}}>
-                    <div className='container'>
+                <header className="App-header" style={{ 'justify-content': 'normal' }}>
+                    <div className='container profile-page-container'>
                         <div className='row justify-content-center'>
                             <div className='col-3'>
                                 <img src={logo} className="img-fluid" alt="logo" />
                             </div>
-                            <div className='col-8'>
-                                <p>This is the profile page for {this.state.username}</p>
-                                <p><b>{this.state.numTagsFollowing}</b> Tags Following
-                                    - <b>{this.state.numFollowers}</b> Followers
-                                    - <b>{this.state.numFollowing}</b> Following</p>
-                                <p style={{ 'overflow-wrap': 'anywhere' }}>My Bio: {this.state.bio}</p>
+                            <div className='col-7'>
+                                <div className='profile-content'>
+                                    <div className='profile-page-header'>
+                                        <h1><b>{this.state.username}</b></h1>
+                                        <p>
+                                            {
+                                                sessionStorage.getItem("username") != null && !this.state.viewingSelf ?
+                                                    <div>
+                                                        <button type="button" class="btn btn-primary">Follow/Unfollow</button>
+                                                        <button type="button" class="btn btn-dark">DM</button>
+                                                        <button type="button" class="btn btn-danger">Block</button>
+                                                    </div>
+                                                    :
+                                                    <div></div>
+                                            }
+                                        </p>
+                                    </div>
+                                    <div className='numbers'>
+                                        <div>
+                                            <p><b>{this.state.numTagsFollowing}</b> Tags</p>
+                                        </div>
+                                        <div>
+                                            <p><b>{this.state.numFollowers}</b> Followers</p>
+                                        </div>
+                                        <div>
+                                            <p><b>{this.state.numFollowing}</b> Following</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <p style={{ 'overflow-wrap': 'anywhere' }}>My Bio: {this.state.bio}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='row activity-row justify-content-center'>
+                            <div className='col-10 ' style={{ 'background-color': 'white' }} >
+                                <p style={{ 'color': 'black' }}>Posts go here</p>
 
                                 {
                                     sessionStorage.getItem("username") == null ?
@@ -58,17 +91,11 @@ class Profile extends React.Component {
                                         this.state.viewingSelf ?
                                             <div>Viewing my own profile</div>
                                             :
-                                            <div>Logged in viewing another profile: follow/unfollow, block, dm</div>
+                                            <div>Logged in viewing another profile</div>
                                 }
                             </div>
                         </div>
-
-                        <div className='row justify-content-center'>
-                            <div className='col-10 ' style={{'background-color': 'white'}} >
-                                <p style={{'color': 'black'}}>Posts go here</p>
-                            </div>
                     </div>
-            </div>
                 </header >
             </div >
         );
