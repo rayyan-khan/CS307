69cce4858ba8c38ae5d505efabae061bf95b7f1c
junaidjaved <JunaidJ.2002@gmail.com>
2022-02-07 17:30:43 -0500

    updated post and homepage to accept backend

--- front-end/src/layouts/homepage.js
+++ front-end/src/layouts/homepage.js
@@ -2,47 +2,67 @@ import { Center, requiredChakraThemeKeys, Stack } from '@chakra-ui/react';
 import React from 'react'
 
 import Post from '../components/feed/post/post';
-import posts from '../components/feed/posts';
 import { useEffect, useState } from 'react';
 
 const axios = require('axios');
 
-
-function postHandler(posts) {
-    return posts.posts.map((post) => {
-        return (
-            <Center pb={5}>
-                <Post
-                    post={post}
-                />
-            </Center>
-        )
+class Homepage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            allPosts: [],
+        }
     }
-    );
-}
 
+    componentDidMount() {
+        this.fetchPosts();
+    }
 
-function Homepage() {
-    useEffect(() => {      
+    fetchPosts() {
+        let allPosts = [];
         try {
             axios.get("http://localhost:5000/api/getOrderedPost")
                 .then(res => {
-                    console.log(res.data);
+                    const posts = res.data
+                    this.setState({ allPosts: posts });
                 })
         } catch (error) {
-            console.log(error);
+            return (
+                <Center pb={5}>
+                    Error
+                </Center>
+            )
         }
-    }, []);
+    }
 
-    return (
-        <div style={{ overflowX: "hidden", overflowY: "scroll", width: "100%", height: "100%" }} >
-            <Center bg={"#151516"} pb={20}>
-                <Stack>
-                    {postHandler(posts)}
-                </Stack>
-            </Center>
-        </div >
-    );
+
+    postHandler() {
+        console.log(this.state.allPosts);
+        return this.state.allPosts.map((post, key) => {
+            return (
+                <Center pb={5}>
+                    <Post
+                        post={post}
+                    />
+                </Center>
+            )
+        }
+        );
+    }
+
+
+
+    render() {
+        return (
+            <div style={{ backgroundColor: "#151516", overflowX: "hidden", overflowY: "scroll", width: "100%", height: "100%" }} >
+                <Center bg={"#151516"} pb={20}>
+                    <Stack>
+                        {this.postHandler()}
+                    </Stack>
+                </Center>
+            </div >
+        );
+    }
 }
 
 export default Homepage;
