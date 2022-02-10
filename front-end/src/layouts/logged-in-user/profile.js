9b40205d6da4fd28cd8c73f2d1a23edf62c168bc
Misha-Rahimi <mlrahimi@purdue.edu>
2022-02-09 22:02:30 -0500

    Profile doesn't flicker with other content

--- front-end/src/layouts/logged-in-user/profile.js
+++ front-end/src/layouts/logged-in-user/profile.js
@@ -17,6 +17,7 @@ class Profile extends React.Component {
             numFollowers: 0,
             viewingSelf: true,
             userExists: true,
+            loading: true,
         }
     }
 
@@ -62,12 +63,14 @@ class Profile extends React.Component {
                         sessionUsername != null &&
                         sessionUsername.localeCompare(this.props.username) ===
                             0,
+                    loading: false,
                 })
             })
             .catch(({ response }) => {
                 if (response.data === "User doesn't exist") {
                     this.setState({
                         userExists: false,
+                        loading: false,
                     })
                 }
             })
@@ -88,154 +91,175 @@ class Profile extends React.Component {
                     height: '100%',
                 }}
             >
-                {this.state.userExists ? (
+                {this.state.loading ? (
+                    <div></div>
+                ) : (
                     <div>
-                        <div className="container justify-content-center profile-page-container">
-                            <div className="row justify-content-center">
-                                <div className="col-3">
-                                    <div class="rect-img-container">
-                                        <img
-                                            class="rect-img rounded-circle"
-                                            src="https://picsum.photos/800/1500"
-                                            alt=""
-                                        />
-                                    </div>
-                                </div>
-                                <div className="col-7">
-                                    <div className="profile-content">
-                                        <div className="profile-page-header">
-                                            <div className="profile-username">
-                                                <h1>
-                                                    <b>{this.state.username}</b>
-                                                </h1>
+                        {this.state.userExists ? (
+                            <div>
+                                <div className="container justify-content-center profile-page-container">
+                                    <div className="row justify-content-center">
+                                        <div className="col-3">
+                                            <div class="rect-img-container">
+                                                <img
+                                                    class="rect-img rounded-circle"
+                                                    src="https://picsum.photos/800/1500"
+                                                    alt=""
+                                                />
                                             </div>
                                         </div>
-                                        <div className="numbers">
-                                            <div>
-                                                <p>
-                                                    <b>
-                                                        {
-                                                            this.state
-                                                                .numTagsFollowing
-                                                        }
-                                                    </b>{' '}
-                                                    Tags
-                                                </p>
-                                            </div>
-                                            <div>
-                                                <p>
-                                                    <b>
-                                                        {
-                                                            this.state
-                                                                .numFollowers
-                                                        }
-                                                    </b>{' '}
-                                                    Followers
-                                                </p>
-                                            </div>
-                                            <div>
-                                                <p>
-                                                    <b>
-                                                        {
-                                                            this.state
-                                                                .numFollowing
-                                                        }
-                                                    </b>{' '}
-                                                    Following
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div className="profile-bio">
-                                            <p
-                                                style={{
-                                                    'overflow-wrap': 'anywhere',
-                                                }}
-                                            >
-                                                My Bio: {this.state.bio}
-                                            </p>
-                                        </div>
-
-                                        <div className="profile-buttons">
-                                            {this.state.viewingSelf ? (
-                                                <div>
-                                                    <button
-                                                        type="button"
-                                                        className="btn btn-primary"
+                                        <div className="col-7">
+                                            <div className="profile-content">
+                                                <div className="profile-page-header">
+                                                    <div className="profile-username">
+                                                        <h1>
+                                                            <b>
+                                                                {
+                                                                    this.state
+                                                                        .username
+                                                                }
+                                                            </b>
+                                                        </h1>
+                                                    </div>
+                                                </div>
+                                                <div className="numbers">
+                                                    <div>
+                                                        <p>
+                                                            <b>
+                                                                {
+                                                                    this.state
+                                                                        .numTagsFollowing
+                                                                }
+                                                            </b>{' '}
+                                                            Tags
+                                                        </p>
+                                                    </div>
+                                                    <div>
+                                                        <p>
+                                                            <b>
+                                                                {
+                                                                    this.state
+                                                                        .numFollowers
+                                                                }
+                                                            </b>{' '}
+                                                            Followers
+                                                        </p>
+                                                    </div>
+                                                    <div>
+                                                        <p>
+                                                            <b>
+                                                                {
+                                                                    this.state
+                                                                        .numFollowing
+                                                                }
+                                                            </b>{' '}
+                                                            Following
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                                <div className="profile-bio">
+                                                    <p
+                                                        style={{
+                                                            'overflow-wrap':
+                                                                'anywhere',
+                                                        }}
                                                     >
-                                                        Edit Profile
-                                                    </button>
+                                                        My Bio: {this.state.bio}
+                                                    </p>
+                                                </div>
+
+                                                <div className="profile-buttons">
+                                                    {this.state.viewingSelf ? (
+                                                        <div>
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-primary"
+                                                            >
+                                                                Edit Profile
+                                                            </button>
+                                                        </div>
+                                                    ) : (
+                                                        <div>
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-primary"
+                                                            >
+                                                                Follow/Unfollow
+                                                            </button>
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-dark"
+                                                            >
+                                                                DM
+                                                            </button>
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-danger"
+                                                            >
+                                                                Block
+                                                            </button>
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="row activity-row justify-content-center">
+                                        <div
+                                            className="col-10 "
+                                            style={{
+                                                'background-color': 'white',
+                                            }}
+                                        >
+                                            <p style={{ color: 'black' }}>
+                                                Posts go here
+                                            </p>
+
+                                            {sessionStorage.getItem(
+                                                'username'
+                                            ) == null ? (
+                                                <div>Not logged in</div>
+                                            ) : this.state.viewingSelf ? (
+                                                <div>
+                                                    Viewing my own profile
                                                 </div>
                                             ) : (
                                                 <div>
-                                                    <button
-                                                        type="button"
-                                                        className="btn btn-primary"
-                                                    >
-                                                        Follow/Unfollow
-                                                    </button>
-                                                    <button
-                                                        type="button"
-                                                        className="btn btn-dark"
-                                                    >
-                                                        DM
-                                                    </button>
-                                                    <button
-                                                        type="button"
-                                                        className="btn btn-danger"
-                                                    >
-                                                        Block
-                                                    </button>
+                                                    Logged in viewing another
+                                                    profile
                                                 </div>
                                             )}
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-
-                            <div className="row activity-row justify-content-center">
-                                <div
-                                    className="col-10 "
-                                    style={{ 'background-color': 'white' }}
-                                >
-                                    <p style={{ color: 'black' }}>
-                                        Posts go here
-                                    </p>
-
-                                    {sessionStorage.getItem('username') ==
-                                    null ? (
-                                        <div>Not logged in</div>
-                                    ) : this.state.viewingSelf ? (
-                                        <div>Viewing my own profile</div>
-                                    ) : (
-                                        <div>
-                                            Logged in viewing another profile
-                                        </div>
-                                    )}
+                                <div className="posts-container">
+                                    {this.postHandler(posts)}
                                 </div>
                             </div>
-                        </div>
-                        <div className="posts-container">
-                            {this.postHandler(posts)}
-                        </div>
-                    </div>
-                ) : (
-                    <div className="container justify-content-center profile-page-container">
-                        <div className="row justify-content-center">
-                            <div className="col-10" style={{ padding: '40px' }}>
-                                <h3
-                                    style={{
-                                        'font-size': '60px',
-                                        'margin-bottom': '20px',
-                                    }}
-                                >
-                                    User Not Found
-                                </h3>
-                                <p>
-                                    The user you were looking for could not be
-                                    found.
-                                </p>
+                        ) : (
+                            <div className="container justify-content-center profile-page-container">
+                                <div className="row justify-content-center">
+                                    <div
+                                        className="col-10"
+                                        style={{ padding: '40px' }}
+                                    >
+                                        <h3
+                                            style={{
+                                                'font-size': '60px',
+                                                'margin-bottom': '20px',
+                                            }}
+                                        >
+                                            User Not Found
+                                        </h3>
+                                        <p>
+                                            The user you were looking for could
+                                            not be found.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 )}
             </div>
