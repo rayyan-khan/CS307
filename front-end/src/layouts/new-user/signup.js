be62bbcd2eeb6398213bfbf38777881c5786ea53
rayyan-khan <rayyan.khan258@gmail.com>
2022-02-13 17:31:26 -0500

    works just has to handle errors returned from axios

--- front-end/src/layouts/new-user/signup.js
+++ front-end/src/layouts/new-user/signup.js
@@ -14,6 +14,7 @@ import { useAnimationFrame } from 'framer-motion';
 const axios = require('axios');
 
 class Login extends React.Component {
+  emailError = false;
   
   constructor(props) {
     super();
@@ -23,9 +24,15 @@ class Login extends React.Component {
         Username:'',
         Password:'',
         PasswordCheck:''
-      }
+      },
+      emailError: false,
+      userError: false,
+      passwordError: false,
+      passwordCheckError: false
     }
 }
+
+
 
 changeHandler=e=>{
   const name = e.target.name;
@@ -46,14 +53,15 @@ registerUser=()=>{
 
   // check if email is valid format
   let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
   if ( re.test(this.state.user.Email) ) {
       // this is a valid email address
       console.log("Valid Email");
+      this.setState({emailError: false})
   }
   else {
       // invalid email, show error
       console.log("Invalid Email");
+      this.setState({emailError: true})
   }
 
   // check that username meets requirements
@@ -63,58 +71,141 @@ registerUser=()=>{
   if(checkUser.test(this.state.user.Username)) {
     // meets requirements
     console.log("Username meets requirements");
+    this.setState({userError: false})
   }
   else {
+    // set username error to true
+    this.setState({userError: true})
+
+    // log reason(s) why its erroring to the console
     let checkUserChar = /^[A-Za-z0-9_.]*$/;
-    if(checkUserChar.test(this.state.user.Username) == false) {
+    if(!checkUserChar.test(this.state.user.Username)) {
       console.log("Username may only contain letters, numbers, underscore or period.");
     }
     let checkUserLen = /.{0, 20}$/;
-    if(checkUserLen.test(this.state.user.Username) == false) {
+    if(!checkUserLen.test(this.state.user.Username)) {
       console.log("Username is too long. Max 20 characters.");
     }
-    
-    // have error pop up
+  }
 
+  // check if password is valid (at least 8 characters 1 letter and 1 number)
+  let checkPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+  if(checkPassword.test(this.state.user.Password)) {
+    console.log("Password meets requirements");
+    this.setState({passwordError: false})
+  }
+  else {
+    console.log("Password does not meet requirements");
+    this.setState({passwordError: true})
   }
 
   // check if passwords match
   if(this.state.user.Password===this.state.user.PasswordCheck) {
     console.log("Passwords Match");
+    this.setState({passwordCheckError: false})
   }
   else {
     // have pop up show passwords do not match
-    console.log("Passwords do not match")
+    console.log("Passwords do not match");
+    this.setState({passwordCheckError: true});
   }
 
-
   console.log(this.state.user);
+
+  // AXIOS STUFF
+  // pass to axios if there are no errors
+  if(this.state.emailError === false && 
+    this.state.userError === false && 
+    this.state.passwordError === false && 
+    this.state.passwordCheckError === false) {
+    console.log("Passed to axios");
+    const payload = {
+      email: this.state.user.Email,
+      username: this.state.user.Username,
+      password: this.state.user.Password
+    }
+    axios.post("http://localhost:5000/api/register", payload)
+      .then((response) => {
+        console.log("got a response");
+        console.log(response.data);
+      })
+      .catch(({response}) => {
+        console.log("got an error");
+        console.log(response.data);
+      })
+  }
+  
 }
 
   render() {   
     return (
       <div className="App">
-        <header className="App-header">
+        <header  className="App-header">  
 
           <h2>Signup</h2>
           <p></p>
           <form>
-          
-          <label>Email: <input type="text" name="Email" 
-                          value={this.state.user.Email} onChange={this.changeHandler} 
-                          style={{ color: 'black'}}></input></label>
-          <p> </p>
-          <label>Username: <input type="text" name="Username"
-                          value={this.state.user.Username} onChange={this.changeHandler}
-                          style={{ color: 'black'}}></input></label>
-          <p> </p>
-          <label>Password: <input type="text" name="Password"
-                          value={this.state.user.Password} onChange={this.changeHandler}
-                          style={{ color: 'black'}}></input></label>
-          <p> </p>
-          <label>Password Check: <input type="text" name="PasswordCheck"
-                          value={this.state.user.PasswordCheck} onChange={this.changeHandler}
-                          style={{ color: 'black'}}></input></label>
+
+          {/* email entry part of form */}
+          <FormControl isInvalid={this.state.emailError}>
+            <FormLabel htmlFor='email'> </FormLabel>
+            <Input 
+              focusBorderColor='teal.200'
+              errorBorderColor='red.300'
+              placeholder = 'Email address' 
+              type = "text" 
+              name="Email" 
+              value={this.state.user.Email} 
+              onChange={this.changeHandler}/>
+              {!this.state.emailError ? ( <FormHelperText> Enter a valid email. </FormHelperText>) 
+              : (<FormErrorMessage>The email you entered is invalid.</FormErrorMessage>)}
+          </FormControl>
+
+          {/* username entry part of form */}
+          <FormControl isInvalid={this.state.userError}>
+          <FormLabel htmlFor='username'> </FormLabel>
+          <Input 
+            focusBorderColor='teal.200'
+            errorBorderColor='red.300'
+            placeholder = 'Username' 
+            type = "text" 
+            name="Username" 
+            value={this.state.user.Username} 
+            onChange={this.changeHandler}/>
+          {!this.state.userError ? ( <FormHelperText> Username must be less then 20 characters and consist of letters, numbers, underscore and period. </FormHelperText>) 
+           : (<FormErrorMessage>The username you entered is invalid.</FormErrorMessage>)}
+          </FormControl>
+
+          {/* password entry part of form */}
+          <FormControl isInvalid={this.state.passwordError}>
+          <FormLabel htmlFor='password'> </FormLabel>
+          <Input 
+            focusBorderColor='teal.200'
+            errorBorderColor='red.300'
+            placeholder = 'Password' 
+            type = "text" 
+            name="Password" 
+            value={this.state.user.Password} 
+            onChange={this.changeHandler}/>
+          {!this.state.passwordError ? ( <FormHelperText> Make a good password if you don't want to get hacked. </FormHelperText>) 
+           : (<FormErrorMessage>Enter a valid password (must be at least 8 characters and have at least 1 letter and 1 number).</FormErrorMessage>)}
+          </FormControl>
+
+          {/* passwordCheck entry part of form */}
+          <FormControl isInvalid={this.state.passwordCheckError}>
+          <FormLabel htmlFor='passwordCheck'> </FormLabel>
+          <Input 
+            focusBorderColor='teal.200'
+            errorBorderColor='red.300'
+            placeholder = 'Retype your password' 
+            type = "text" 
+            name="PasswordCheck" 
+            value={this.state.user.PasswordCheck} 
+            onChange={this.changeHandler}/>
+          {!this.state.passwordCheckError ? ( <FormHelperText>Make sure this password matches the one above. </FormHelperText>) 
+           : (<FormErrorMessage>Your password doesn't match the one above.</FormErrorMessage>)}
+          </FormControl>
+
         </form>
         <button onClick={this.registerUser}>Submit</button>
         </header>
