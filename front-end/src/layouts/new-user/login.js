34257ffde840763013931bf004b2ff5045d21228
rayyan-khan <rayyan.khan258@gmail.com>
2022-02-14 14:03:57 -0500

    added imports

--- front-end/src/layouts/new-user/login.js
+++ front-end/src/layouts/new-user/login.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Badge,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  Button,
+  PopoverAnchor,
+  transform
+} from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 
 import logo from '../../logo.png';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useAnimationFrame } from 'framer-motion';
 
 class Login extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This is the login page
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: "#C5B1F5" }}
-          >
-            Learn React
-          </a>
+          
+        
+
         </header>
       </div>
     );
