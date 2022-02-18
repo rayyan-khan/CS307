257dc72fdd5a43f6f72b348bf3a6d23eaafb6cd7
Misha-Rahimi <mlrahimi@purdue.edu>
2022-02-17 20:58:17 -0500

    File upload issue fixed for specific machine

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,74 +1,92 @@
-const express = require('express');
-const postRoutes = express.Router();
+const express = require('express')
+const postRoutes = express.Router()
 //const s3 = require("../s3Bucket/create-bucket")
 
-
-
 //Use the below route to create a post and store into the database and S3
-const s3 = require("../s3Bucket/create-bucket")
+const s3 = require('../s3Bucket/create-bucket')
 //
 const multer = require('multer')
-var storage = multer.diskStorage(
-    {
-        destination: './uploads/',
-        filename: function (req, file, cb) {
-            console.log(file.originalname)
-            cb(null, file.originalname + ".jpg");
-        }
-    }
-);
-
-var upload = multer( { storage: storage } );
-postRoutes.route("/posts/postImage").post( upload.single('image'), async function (req, res) {
-  //  var url = s3.uploadFile(req.file);
-
-    //get username
-    // var user
-    console.log(req.body)
-    // try {
-    //     //Use decodeHeader to extract user info from header or throw an error
-    //    // user = await decodeHeader.decodeAuthHeader(req)
-    // } catch (err) {
-       
-    //     return res.status(400).json(err)
-    // }
-
-
- //   const { email, username } = user
-   // console.log(username);
-    //
-    var getId = "Select Max(postID) as ID From Post;"
-    var Is;
-    con.query(getId, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(500).json(err);
-        } else res.json(result)
-        console.log(result[0].ID);
-        Is = result[0].ID
-        Is += 1//store the ID
-        //check if file is okay to store
-
-        //
-        var url = s3.uploadFile(req.file);
-        url = "https://cs307.s3.amazonaws.com/" + req.file.path
-        console.log(url);
-        var sql = "INSERT INTO Post Values ('" + Is+ "', '" + Is + "', '" +"yoloooo"+ "', '" +"12', '14" +"', '" + req.body.caption+"', NOW(),'12"+"', '" + req.body.anonymous+"', '" +url+"', '" +req.body.hyperlink+ "')";
-        //    var sql = "INSERT INTO Post Values (20,12,'ak',12,'12','12',NOW(),'12','1');"
-        con.query(sql, function (err, results) {
-            if (err) throw err;
-            console.log("1 record inserted");
-            console.log(results)
-        });
-
-    })
-    //
-    // // console.log(req.file)
-    // // s3.uploadFile(req.file.path);
-    // // res.json("user added")
+var storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function (req, file, cb) {
+        console.log(file.originalname)
+        cb(null, file.originalname + '.jpg')
+    },
 })
 
-postRoutes.route("/posts/postNoImage").post(  async function (req, res) {
+var upload = multer({ storage: storage })
+postRoutes
+    .route('/posts/postImage')
+    .post(upload.single('image'), async function (req, res) {
+        //  var url = s3.uploadFile(req.file);
+
+        //get username
+        // var user
+        console.log(req.body)
+        // try {
+        //     //Use decodeHeader to extract user info from header or throw an error
+        //    // user = await decodeHeader.decodeAuthHeader(req)
+        // } catch (err) {
+
+        //     return res.status(400).json(err)
+        // }
+
+        //   const { email, username } = user
+        // console.log(username);
+        //
+        var getId = 'Select Max(postID) as ID From Post;'
+        var Is
+        con.query(getId, async (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(500).json(err)
+            } else res.json(result)
+            console.log(result[0].ID)
+            Is = result[0].ID
+            Is += 1 //store the ID
+            //check if file is okay to store
+
+            //
+            //var url = 'garbage for a sec'
+            s3.uploadFile(req.file).then((url) => {
+                console.log('promise result ' + res)
+
+                //url = "https://cs307.s3.amazonaws.com/" + req.file.path.substring(8)
+                console.log(url)
+                var sql =
+                    "INSERT INTO Post Values ('" +
+                    Is +
+                    "', '" +
+                    Is +
+                    "', '" +
+                    'yoloooo' +
+                    "', '" +
+                    "12', '14" +
+                    "', '" +
+                    req.body.caption +
+                    "', NOW(),'12" +
+                    "', '" +
+                    req.body.anonymous +
+                    "', '" +
+                    url +
+                    "', '" +
+                    req.body.hyperlink +
+                    "')"
+                //    var sql = "INSERT INTO Post Values (20,12,'ak',12,'12','12',NOW(),'12','1');"
+                con.query(sql, function (err, results) {
+                    if (err) throw err
+                    console.log('1 record inserted')
+                    console.log(results)
+                })
+            })
+        })
+        //
+        // // console.log(req.file)
+        // // s3.uploadFile(req.file.path);
+        // // res.json("user added")
+    })
+
+postRoutes.route('/posts/postNoImage').post(async function (req, res) {
     //  var url = s3.uploadFile(req.file);
 
     //get username
@@ -82,36 +100,51 @@ postRoutes.route("/posts/postNoImage").post(  async function (req, res) {
     //     return res.status(400).json(err)
     // }
 
-
     //   const { email, username } = user
     // console.log(username);
     //
 
-    var getId = "Select Max(postID) as ID From Post;"
-    var Is;
+    var getId = 'Select Max(postID) as ID From Post;'
+    var Is
     con.query(getId, function (err, result) {
-        if (err){
-            console.log(err);
-            res.status(500).json(err);
+        if (err) {
+            console.log(err)
+            res.status(500).json(err)
         } else res.json(result)
-        console.log(result[0].ID);
+        console.log(result[0].ID)
         Is = result[0].ID
-        Is+=1//store the ID
-        var sql = "INSERT INTO Post Values ('" + Is+ "', '" + Is + "', '" +"yoloooo"+ "', '" +"12', '14" +"', '" + req.body.caption+"', NOW(),'12"+"', '" + req.body.anonymous+"', '" +"undefined"+"', '" +req.body.hyperlink+ "')";
+        Is += 1 //store the ID
+        var sql =
+            "INSERT INTO Post Values ('" +
+            Is +
+            "', '" +
+            Is +
+            "', '" +
+            'yoloooo' +
+            "', '" +
+            "12', '14" +
+            "', '" +
+            req.body.caption +
+            "', NOW(),'12" +
+            "', '" +
+            req.body.anonymous +
+            "', '" +
+            'undefined' +
+            "', '" +
+            req.body.hyperlink +
+            "')"
         //    var sql = "INSERT INTO Post Values (20,12,'ak',12,'12','12',NOW(),'12','1');"
         con.query(sql, function (err, results) {
-            if (err) throw err;
-            console.log("1 record inserted");
+            if (err) throw err
+            console.log('1 record inserted')
             console.log(results)
-        });
-
+        })
     })
     //
     // // console.log(req.file)
     // // s3.uploadFile(req.file.path);
     // // res.json("user added")
 })
-
 
 // var upload = multer( { storage: storage } );
 // userRoutes.route("/profile").post( upload.single('image'), function (req, res) {
@@ -130,55 +163,54 @@ postRoutes.route("/posts/postNoImage").post(  async function (req, res) {
 // })
 
 //
-postRoutes.route("/getSpecificPost/:postID").post(function (req, res) {
-    var sql = "SELECT * From Post WHERE postId = '" + req.params.postID + "'";
+postRoutes.route('/getSpecificPost/:postID').post(function (req, res) {
+    var sql = "SELECT * From Post WHERE postId = '" + req.params.postID + "'"
     con.query(sql, function (err, result) {
         if (err) {
-            console.log(err);
-            res.status(500).json(err);
+            console.log(err)
+            res.status(500).json(err)
         } else res.json(result)
     })
 })
 
-
 //Use the below line in any file to connect to the database
-var con = require("../database/conn");
+var con = require('../database/conn')
 //use the below route to get information on a specific post
-postRoutes.route("/getSpecificPost/:postID").get(function (req, res) {
-    var sql = "SELECT * From Post WHERE postId = '" + req.params.postID + "'";
+postRoutes.route('/getSpecificPost/:postID').get(function (req, res) {
+    var sql = "SELECT * From Post WHERE postId = '" + req.params.postID + "'"
     con.query(sql, function (err, result) {
         if (err) {
-            console.log(err);
-            res.status(500).json(err);
+            console.log(err)
+            res.status(500).json(err)
         } else res.json(result)
     })
 })
 
 //use the below route to get all the posts in order of time posted
-postRoutes.route("/getOrderedPost").get(function (req, res) {
-    var sql = "SELECT * From Post Order BY timeStamp DESC";
+postRoutes.route('/getOrderedPost').get(function (req, res) {
+    var sql = 'SELECT * From Post Order BY timeStamp DESC'
 
     con.query(sql, function (err, result) {
         if (err) {
-            console.log(err);
-            res.status(500).json(err);
+            console.log(err)
+            res.status(500).json(err)
         } else res.json(result)
     })
 })
 
-module.exports = postRoutes;
+module.exports = postRoutes
 
 //userRoutes.route("/testing").post(function (req, res) {
-  //  console.log(req.body);
- //   var sql = "INSERT INTO User (username, password) VALUES ('" + req.body.username + "', '" + req.body.password + "')";
+//  console.log(req.body);
+//   var sql = "INSERT INTO User (username, password) VALUES ('" + req.body.username + "', '" + req.body.password + "')";
 
-  //  con.query(sql, function (err, result) {
- //       if (err) throw err;
+//  con.query(sql, function (err, result) {
+//       if (err) throw err;
 //        console.log("1 record inserted");
 //        console.log(result)
 //    });
 
-  //  res.json("user added")/
+//  res.json("user added")/
 //});
 //
 // userRoutes.route("/exists/:username").get(function (req, res) {
--- server/s3Bucket/create-bucket.js
+++ server/s3Bucket/create-bucket.js
@@ -1,42 +1,44 @@
-const fs = require('fs');
-const AWS = require('aws-sdk');
-
+const fs = require('fs')
+const AWS = require('aws-sdk')
 
 // Enter copied or downloaded access id and secret here
-const ID = 'AKIA5D526RAUHAYT7D76';
-const SECRET = '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';
+const ID = 'AKIA5D526RAUHAYT7D76'
+const SECRET = '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'
 
 // Enter the name of the bucket that you have created here
-const BUCKET_NAME = 'cs307';
-
+const BUCKET_NAME = 'cs307'
 
 // Initializing S3 Interface
 const s3 = new AWS.S3({
     accessKeyId: ID,
-    secretAccessKey: SECRET
-});
+    secretAccessKey: SECRET,
+})
 
-module.exports.uploadFile = (fileName) => {
-    // read content from the file
-    const fileContent = fs.readFileSync(fileName.path);
-   // console.log(fileName.path);
-  //  console.log(fileName.originalname);
-    //const k = keyName
+module.exports.uploadFile = async (fileName) => {
+    return new Promise((resolve, reject) => {
+        // read content from the file
+        const fileContent = fs.readFileSync(fileName.path)
+        // console.log(fileName.path);
+        //  console.log(fileName.originalname);
+        //const k = keyName
 
-    // setting up s3 upload parameters
-    let params = {
-        Bucket: BUCKET_NAME,
-        Key: fileName.path , // file name you want to save as
-        Body: fileContent
-    };
-
-    // Uploading files to the bucket
-    s3.upload(params, function(err, data) {
-        if (err) {
-            throw err
+        // setting up s3 upload parameters
+        let params = {
+            Bucket: BUCKET_NAME,
+            Key: fileName.path.substring(8), // file name you want to save as
+            Body: fileContent,
         }
-        console.log(`File uploaded successfully. ${data.Location}`)
-    });
-};
+
+        // Uploading files to the bucket
+        s3.upload(params, async (err, data) => {
+            if (err) {
+                reject(err)
+            }
+            console.log(`File uploaded successfully. ${data.Location}`)
+
+            resolve(data.Location)
+        })
+    })
+}
 
 // Enter the file you want to upload here\
