fd54679cbd2e591be25c1c192813975558c2e7fe
rayyan-khan <rayyan.khan258@gmail.com>
2022-03-20 03:36:33 -0400

    reset password page not displaying

---
+++ front-end/src/layouts/new-user/reset-password.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Center>
+              <Text fontSize={'4xl'} style={{ color: 'mediumturquoise' }}>Reset Your Password</Text>
+        </Center>
+      </div>
+    );
+  }
+}
+export default Login;
