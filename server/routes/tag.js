76d3b70570ee456487c2c54156bdb20425b7747d
ClaySpike <povinellimax@gmail.com>
2022-03-22 16:30:11 -0400

    Created Tag basics

---
+++ server/routes/tag.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const tagRoutes = express.Router()
+const decodeHeader = require('../utils/decodeHeader')
+
+//Use the below line in any file to connect to the database
+var getCon = require('../database/conn')
+var con = getCon.getConObject()
+
+tagRoutes.route('/getTags').get(async (req, res) => {
+    var sql = `SELECT * from Tag`
+
+    con.query(sql, function (err, fullResponse) {
+        if (fullResponse.length === 0)
+            return res.status(400).json("Tags doesn't exist")
+        let result = fullResponse[0]
+        console.log(result)
+        if (err) {
+            console.log(result)
+            return res.status(500).json(err)
+        }
+
+        res.status(200).json(result)
+    })
+})
+
+tagRoutes.route('/createTag/:tagName').get(async (req, res) => {
+    var sql = `INSERT INTO Tag VALUES (${con.escape(req.params.tagName)}, 0)`
+
+    con.query(sql, function (err, fullResponse) {
+        if (fullResponse.length === 0)
+            return res.status(400).json("Tags doesn't exist")
+        let result = fullResponse[0]
+        console.log(result)
+        if (err) {
+            console.log(result)
+            return res.status(500).json(err)
+        }
+
+        res.status(200).json(result)
+    })
+})
+
+module.exports = tagRoutes
