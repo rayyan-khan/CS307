305bd13c3c3f5027d30235a58b7d97ccdd539565
Misha-Rahimi <mlrahimi@purdue.edu>
2022-04-06 19:30:53 -0400

    Search bar in DM's updates talking with username

--- front-end/src/layouts/logged-in-user/dm's/direct-message.js
+++ front-end/src/layouts/logged-in-user/dm's/direct-message.js
@@ -1,164 +1,219 @@
 import axios from 'axios'
 import React from 'react'
 import Post from '../../../components/feed/post/post'
+import SearchBar from './searchBar'
 import {
-  Box,
-  Button,
-  Center,
-  Image,
-  Stack,
-  Text,
-  Input,
-  IconButton,
-  Avatar,
-  Tooltip,
-  Grid,
-  GridItem
+    Box,
+    Button,
+    Center,
+    Image,
+    Stack,
+    Text,
+    Input,
+    IconButton,
+    Avatar,
+    Tooltip,
+    Grid,
+    GridItem,
 } from '@chakra-ui/react'
 import '../../layouts.css'
 
 const tempConversations = [
-  {
-    "username": "Misha",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "20m",
-  },
-  {
-    "username": "Atharva",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "1h",
-
-  },
-  {
-    "username": "Rayyan",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "3h",
-  },
-  {
-    "username": "Misha",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "12h",
-  },
-  {
-    "username": "Atharva",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "2d",
-
-  },
-  {
-    "username": "Rayyan",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "4d",
-  }, {
-    "username": "Misha",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "1w",
-  },
-  {
-    "username": "Atharva",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "3w",
-
-  },
-  {
-    "username": "Rayyan",
-    "avatar": "https://i.imgur.com/qJHvZ9x.png",
-    "lastMessage": "Hey, how are you?",
-    "time": "5w",
-  },
+    {
+        username: 'Misha',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '20m',
+    },
+    {
+        username: 'Atharva',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '1h',
+    },
+    {
+        username: 'Rayyan',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '3h',
+    },
+    {
+        username: 'Misha',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '12h',
+    },
+    {
+        username: 'Atharva',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '2d',
+    },
+    {
+        username: 'Rayyan',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '4d',
+    },
+    {
+        username: 'Misha',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '1w',
+    },
+    {
+        username: 'Atharva',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '3w',
+    },
+    {
+        username: 'Rayyan',
+        avatar: 'https://i.imgur.com/qJHvZ9x.png',
+        lastMessage: 'Hey, how are you?',
+        time: '5w',
+    },
 ]
 
 class DirectMessage extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      talkingToUsername: this.props.usernameToTalkWith ? this.props.usernameToTalkWith : "",
-    };
-  }
-
-  componentDidMount() {
-    if (axios.defaults.headers.common['authorization'] != null) {
-      axios.get('http://localhost:5000/api/getUserFromHeader').then((res) => {
-        this.setState({ username: res.data.username })
-      })
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            talkingToUsername: this.props.usernameToTalkWith
+                ? this.props.usernameToTalkWith
+                : '',
+        }
     }
-  }
 
+    componentDidMount() {
+        if (axios.defaults.headers.common['authorization'] != null) {
+            axios
+                .get('http://localhost:5000/api/getUserFromHeader')
+                .then((res) => {
+                    this.setState({ username: res.data.username })
+                })
+        }
+    }
 
-
-  render() {
-    return (
-      <Grid templateColumns='repeat(2, 1fr)'>
-        <GridItem height={"100vh"}>
-          <Box>
-            <Stack borderRightWidth={'2px'} overflowX={'hidden'} overflowY={'scroll'} width={'25vw'} ml={5} direction={'column'} height={'94vh'}>
-              <Box pt={10} width={'27vw'} overflowX={'hidden'} overflowY={'auto'}>
-                {
-                  tempConversations.map((conversation, key) => {
-                    return (
-                      <Box
-                        mb={8}
-                        ml={8}
-                        width={'80%'}
-                        maxWidth={'400px'}
-                        boxShadow={'2xl'}
-                        rounded={'lg'}
-                        _hover={{
-                          boxShadow: '2xl',
-                          borderColor: 'darkturquoise',
-                          borderWidth: '2px',
-                          transform: 'scale(1.05)',
-                          transition: 'all 0.2s ease-in-out',
-
-                        }}
-                        onClick={() => {
-                          this.setState({ talkingToUsername: conversation.username })
-                        }}
-                      >
-                        <Stack p={8} direction={'row'}>
-                          <Avatar
-                            name={conversation.username}
-                            src={conversation.avatar}
-                            size="md"
-                            mr={2}
-                          />
-                          <Stack direction={'column'}>
-                            <Text color={'darkturquoise'} fontSize={'md'}>{conversation.username}</Text>
-                            <Text overflow={'clip'} color={'var(--text-color)'} fontSize={'md'}>{conversation.lastMessage}</Text>
-                          </Stack>
-                          <Box width={'100px'} textAlign={'right'}>
-                            <Text color={'var(--text-color)'} right={0} pl={6} pt={5}>
-                              {conversation.time}
-                            </Text>
-                          </Box>
+    render() {
+        return (
+            <Grid templateColumns="repeat(2, 1fr)">
+                <GridItem height={'100vh'}>
+                    <Box>
+                        <Stack
+                            borderRightWidth={'2px'}
+                            overflowX={'hidden'}
+                            overflowY={'scroll'}
+                            width={'25vw'}
+                            ml={5}
+                            direction={'column'}
+                            height={'94vh'}
+                        >
+                            <Box
+                                pt={10}
+                                width={'27vw'}
+                                overflowX={'hidden'}
+                                overflowY={'auto'}
+                            >
+                                <div
+                                    style={{
+                                        width: '75%',
+                                        marginLeft: '8%',
+                                        marginBottom: '20%',
+                                    }}
+                                >
+                                    <SearchBar
+                                        updateConversation={(username) => {
+                                            this.setState({
+                                                talkingToUsername: username,
+                                            })
+                                        }}
+                                    />
+                                </div>
+                                {tempConversations.map((conversation, key) => {
+                                    return (
+                                        <Box
+                                            mb={8}
+                                            ml={8}
+                                            width={'80%'}
+                                            maxWidth={'400px'}
+                                            boxShadow={'2xl'}
+                                            rounded={'lg'}
+                                            _hover={{
+                                                boxShadow: '2xl',
+                                                borderColor: 'darkturquoise',
+                                                borderWidth: '2px',
+                                                transform: 'scale(1.05)',
+                                                transition:
+                                                    'all 0.2s ease-in-out',
+                                            }}
+                                            onClick={() => {
+                                                this.setState({
+                                                    talkingToUsername:
+                                                        conversation.username,
+                                                })
+                                            }}
+                                        >
+                                            <Stack p={8} direction={'row'}>
+                                                <Avatar
+                                                    name={conversation.username}
+                                                    src={conversation.avatar}
+                                                    size="md"
+                                                    mr={2}
+                                                />
+                                                <Stack direction={'column'}>
+                                                    <Text
+                                                        color={'darkturquoise'}
+                                                        fontSize={'md'}
+                                                    >
+                                                        {conversation.username}
+                                                    </Text>
+                                                    <Text
+                                                        overflow={'clip'}
+                                                        color={
+                                                            'var(--text-color)'
+                                                        }
+                                                        fontSize={'md'}
+                                                    >
+                                                        {
+                                                            conversation.lastMessage
+                                                        }
+                                                    </Text>
+                                                </Stack>
+                                                <Box
+                                                    width={'100px'}
+                                                    textAlign={'right'}
+                                                >
+                                                    <Text
+                                                        color={
+                                                            'var(--text-color)'
+                                                        }
+                                                        right={0}
+                                                        pl={6}
+                                                        pt={5}
+                                                    >
+                                                        {conversation.time}
+                                                    </Text>
+                                                </Box>
+                                            </Stack>
+                                        </Box>
+                                    )
+                                })}
+                            </Box>
                         </Stack>
-                      </Box>
-                    )
-                  })
-                }
-              </Box>
-            </Stack>
-          </Box>
-        </GridItem>
-        <GridItem>
-          <Box>
-            <Text color={'var(--text-color)'} size={'4xl'}>
-              {this.state.username} wants to talk with {this.state.talkingToUsername}
-            </Text>
-          </Box>
-        </GridItem>
-      </Grid >
-    );
-  }
+                    </Box>
+                </GridItem>
+                <GridItem>
+                    <Box>
+                        <Text color={'var(--text-color)'} size={'4xl'}>
+                            {this.state.username} wants to talk with{' '}
+                            {this.state.talkingToUsername}
+                        </Text>
+                    </Box>
+                </GridItem>
+            </Grid>
+        )
+    }
 }
-export default DirectMessage;
+export default DirectMessage
---
+++ front-end/src/layouts/logged-in-user/dm's/searchBar.js
@@ -0,0 +1,93 @@
+import React, { Component, Fragment } from 'react'
+import Select from 'react-select'
+import axios from 'axios'
+import FormatOptionLabel from '../../../components/option'
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            searchableNames: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            searchableNames: [],
+        })
+    }
+
+    onChange = (input) => {
+        console.log('chose person')
+
+        this.props.updateConversation(input.value)
+    }
+
+    onInputChange = (input) => {
+        if (input === '') {
+            console.log('empty value, resetting searchable names')
+            this.setState({ searchableNames: [] })
+        } else {
+            axios
+                .get('http://localhost:5000/api/search/' + input)
+                .then((response) => {
+                    let searchableNames = response.data.filter(
+                        (option) => option.type === 'user'
+                    )
+                    this.setState({
+                        searchableNames: searchableNames,
+                    })
+                })
+                .catch((err) => {
+                    console.log('error')
+                    this.setState({ searchableNames: [] })
+                })
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Select
+                    className="basic-single"
+                    classNamePrefix="select"
+                    isClearable
+                    isSearchable
+                    name="color"
+                    options={this.state.searchableNames}
+                    getOptionLabel={(option) => {
+                        if (option.type === 'user') {
+                            return `${option.label} @${option.value}`
+                        } else {
+                            return `r/${option.label}`
+                        }
+                    }}
+                    formatOptionLabel={FormatOptionLabel}
+                    onChange={this.onChange}
+                    onInputChange={this.onInputChange}
+                    placeholder="Search"
+                    styles={{
+                        control: (base) => ({
+                            ...base,
+                            height: 35,
+                            minHeight: 35,
+                        }),
+                        dropdownIndicator: (styles) => ({
+                            ...styles,
+                            paddingTop: 7,
+                            paddingBottom: 7,
+                        }),
+                        clearIndicator: (styles) => ({
+                            ...styles,
+                            paddingTop: 7,
+                            paddingBottom: 7,
+                        }),
+                    }}
+                />
+            </Fragment>
+        )
+    }
+}
+
+export default SearchBar
