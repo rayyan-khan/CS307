f5ba3903d83cc97172ad4c99a13cff76ec619a05
junaidjaved <JunaidJ.2002@gmail.com>
2022-04-07 09:57:20 -0400

    added some transitions to direct messages

--- front-end/src/layouts/logged-in-user/dm's/direct-message.js
+++ front-end/src/layouts/logged-in-user/dm's/direct-message.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import Post from '../../../components/feed/post/post'
 import SearchBar from './searchBar'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 import {
     Box,
     Button,
@@ -17,6 +19,8 @@ import {
     GridItem,
 } from '@chakra-ui/react'
 import '../../layouts.css'
+import './direct-message.css'
+
 
 const tempConversations = [
     {
@@ -38,40 +42,16 @@ const tempConversations = [
         time: '3h',
     },
     {
-        username: 'Misha',
+        username: 'Max',
         avatar: 'https://i.imgur.com/qJHvZ9x.png',
         lastMessage: 'Hey, how are you?',
-        time: '12h',
+        time: '3h',
     },
     {
-        username: 'Atharva',
+        username: 'Bhavey',
         avatar: 'https://i.imgur.com/qJHvZ9x.png',
         lastMessage: 'Hey, how are you?',
-        time: '2d',
-    },
-    {
-        username: 'Rayyan',
-        avatar: 'https://i.imgur.com/qJHvZ9x.png',
-        lastMessage: 'Hey, how are you?',
-        time: '4d',
-    },
-    {
-        username: 'Misha',
-        avatar: 'https://i.imgur.com/qJHvZ9x.png',
-        lastMessage: 'Hey, how are you?',
-        time: '1w',
-    },
-    {
-        username: 'Atharva',
-        avatar: 'https://i.imgur.com/qJHvZ9x.png',
-        lastMessage: 'Hey, how are you?',
-        time: '3w',
-    },
-    {
-        username: 'Rayyan',
-        avatar: 'https://i.imgur.com/qJHvZ9x.png',
-        lastMessage: 'Hey, how are you?',
-        time: '5w',
+        time: '3h',
     },
 ]
 
@@ -146,7 +126,7 @@ class DirectMessage extends React.Component {
                                     style={{
                                         width: '75%',
                                         marginLeft: '8%',
-                                        marginBottom: '20%',
+                                        marginBottom: '10%',
                                     }}
                                 >
                                     <SearchBar
@@ -157,76 +137,80 @@ class DirectMessage extends React.Component {
                                         }}
                                     />
                                 </div>
-                                {this.state.conversations.map((conversation, key) => {
-                                    return (
-                                        <Box
-                                            mb={8}
-                                            ml={8}
-                                            backgroundColor={conversation.username == this.state.talkingToUsername ? 'darkturquoise' : 'var(--main-color)'}
-                                            width={'80%'}
-                                            maxWidth={'400px'}
-                                            boxShadow={'2xl'}
-                                            rounded={'lg'}
-                                            _hover={{
-                                                boxShadow: '2xl',
-                                                borderColor: 'darkturquoise',
-                                                borderWidth: '2px',
-                                                transform: 'scale(1.05)',
-                                                transition:
-                                                    'all 0.2s ease-in-out',
-                                            }}
-                                            onClick={() => {
-                                                this.setState({
-                                                    talkingToUsername:
-                                                        conversation.username,
-                                                })
-                                            }}
-                                        >
-                                            <Stack p={8} direction={'row'}>
-                                                <Avatar
-                                                    name={conversation.username}
-                                                    src={conversation.avatar}
-                                                    size="md"
-                                                    mr={2}
-                                                />
-                                                <Stack direction={'column'}>
-                                                    <Text
-                                                        color={conversation.username == this.state.talkingToUsername ? 'var(--main-color)' : 'darkturquoise'}
-                                                        fontSize={'md'}
-                                                    >
-                                                        {conversation.username}
-                                                    </Text>
-                                                    <Text
-                                                        overflow={'clip'}
-                                                        color={
-                                                            'var(--text-color)'
-                                                        }
-                                                        fontSize={'md'}
-                                                    >
-                                                        {
-                                                            conversation.lastMessage
-                                                        }
-                                                    </Text>
-                                                </Stack>
+                                <TransitionGroup component="Box">
+                                    {this.state.conversations.map((conversation, key) => {
+                                        return (
+                                            <CSSTransition key={conversation.username} timeout={700} classNames="conversation">
                                                 <Box
-                                                    width={'100px'}
-                                                    textAlign={'right'}
+                                                    mb={8}
+                                                    ml={8}
+                                                    backgroundColor={conversation.username == this.state.talkingToUsername ? 'darkturquoise' : 'var(--main-color)'}
+                                                    width={'80%'}
+                                                    maxWidth={'400px'}
+                                                    boxShadow={'xl'}
+                                                    rounded={'lg'}
+                                                    _hover={{
+                                                        boxShadow: '2xl',
+                                                        borderColor: 'darkturquoise',
+                                                        borderWidth: '2px',
+                                                        transform: 'scale(1.05)',
+                                                        transition:
+                                                            'all 0.2s ease-in-out',
+                                                    }}
+                                                    onClick={() => {
+                                                        this.setState({
+                                                            talkingToUsername:
+                                                                conversation.username,
+                                                        })
+                                                    }}
                                                 >
-                                                    <Text
-                                                        color={
-                                                            'var(--text-color)'
-                                                        }
-                                                        right={0}
-                                                        pl={6}
-                                                        pt={5}
-                                                    >
-                                                        {conversation.time}
-                                                    </Text>
+                                                    <Stack p={8} direction={'row'}>
+                                                        <Avatar
+                                                            name={conversation.username}
+                                                            src={conversation.avatar}
+                                                            size="md"
+                                                            mr={2}
+                                                        />
+                                                        <Stack direction={'column'}>
+                                                            <Text
+                                                                color={conversation.username == this.state.talkingToUsername ? 'var(--main-color)' : 'darkturquoise'}
+                                                                fontSize={'md'}
+                                                            >
+                                                                {conversation.username}
+                                                            </Text>
+                                                            <Text
+                                                                overflow={'clip'}
+                                                                color={
+                                                                    'var(--text-color)'
+                                                                }
+                                                                fontSize={'md'}
+                                                            >
+                                                                {
+                                                                    conversation.lastMessage
+                                                                }
+                                                            </Text>
+                                                        </Stack>
+                                                        <Box
+                                                            width={'100px'}
+                                                            textAlign={'right'}
+                                                        >
+                                                            <Text
+                                                                color={
+                                                                    'var(--text-color)'
+                                                                }
+                                                                right={0}
+                                                                pl={6}
+                                                                pt={5}
+                                                            >
+                                                                {conversation.time}
+                                                            </Text>
+                                                        </Box>
+                                                    </Stack>
                                                 </Box>
-                                            </Stack>
-                                        </Box>
-                                    )
-                                })}
+                                            </CSSTransition>
+                                        )
+                                    })}
+                                </TransitionGroup>
                             </Box>
                         </Stack>
                     </Box>
